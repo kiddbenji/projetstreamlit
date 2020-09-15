f58ce36aa349b40bc92d132704344d282785adee
benjamin conte <benjamin.conte@outlook.fr>
2020-09-15 16:07:55 +0200

    pour streamlit

--- app.py
+++ app.py
@@ -1,25 +1,67 @@
 import streamlit as st
+from PIL import Image
 
-st.markdown("""
-    # POLITICAL PREDICTOR
-    Political Predictor is an application that predicts the European group corresponding to a given ideology based on the twitter account of 473 deputies.
-""")
-st.markdown("""
-    ## **DATA**
-    - Political group
-    - National Party
-    - Country
-    - Age
-    - Gender(0: man, 1: woman)
-    - tweets
-""")
+img = 'delphes.jpg'
+
+
+
+
+
+
 
 
 st.markdown("""
-    ## ****
+    # Project DELPHES
+    Project DELPHES is an application that predicts the European group corresponding to a given ideology based on the twitter account of 472 deputies.
 """)
+
+
+image = Image.open(img)
+st.image(image, use_column_width=True)
+
+target = ('country', 'deputies name', 'gender', 'political group')
+output = st.sidebar.selectbox("You have to chose the target!", target)
+st.sidebar.write(f'You chosed the {output} for the prediction.')
+
+
+input_type = ("sentence", 'twitter account', 'article')
+inp = st.sidebar.selectbox("You do have to chose a input type!", input_type)
+st.sidebar.write(f'You chosed the {inp} to make the prediction.')
+
+
+def get_result(output):
+    if output == "deputies name":
+        return "deputy name", "url of deputies"
+    elif output == "political group":
+        return "political group", "accuracy", "Url political group"
+    elif output == "gender":
+        return "gender" , "accuracy"
+    else:
+        return "country_name"
 
 
 st.markdown("""
-    ## **MODELS**
+    ## **Results**
 """)
+
+text = st.text_input( 'Enter a sentence')
+
+
+
+import time
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+f'and the results is ...{get_result(output)}'
+
+    
